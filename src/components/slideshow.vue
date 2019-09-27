5b1e030dfaf1403d7f28586927ce30e4123bcaaa
CAOHONGQING\Administrator <caocoder@gmail.com>
2019-09-27 12:25:46 +0800

    【完成基础布局以及轮播切换效果】

--- src/components/slideshow.vue
+++ src/components/slideshow.vue
@@ -1,29 +1,84 @@
 <template>
   <div class="slideshow-warpper">
-  	<div class="img-warpper">
-  	  <div class="border-img"></div>
-  	  <div class="head-img"></div>
-  	</div>
-  	<a class="btn"></a>
-  	<a class="btn next"></a>
+    <div class="img-warpper">
+      <div class="border-img">
+        <img :src="borderImgList[nowIndex].src" />
+      </div>
+      <div class="head-img">
+        <img src="../assets/test.jpg" />
+      </div>
+    </div>
+    <div class="btn" @click="goto(prevIndex)"></div>
+    <div class="btn next" @click="goto(prevIndex)"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "slideshow",
-  props: []
+  props: ["borderImgList"],
+  data() {
+    return {
+      nowIndex: 0,
+      ImgList: this.borderImgList[0].src
+    };
+  },
+  methods: {
+    goto(index) {
+      this.nowIndex = index;
+      this.ImgList = this.borderImgList[index].src;
+    }
+  },
+  computed: {
+    prevIndex() {
+      if (this.nowIndex === 0) {
+        return this.borderImgList.length - 1;
+      } else {
+        return this.nowIndex - 1;
+      }
+    },
+    nextIndex() {
+      if (this.nowIndex === this.borderImgList.length - 1) {
+        return 0;
+      } else {
+        return this.nowIndex + 1;
+      }
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-  .slideshow-warpper{
-  	padding: 32px;
-  	position: relative;
-  	.btn{
-  		position:absolute;
-  		top: 50%;
-  		left: 0;
-  	}
+.slideshow-warpper {
+  padding: 40px;
+  position: relative;
+  .img-warpper {
+    width: 150px;
+    height: 150px;
+    margin: 0 auto;
+    position: relative;
+    .border-img {
+      position: absolute;
+      z-index: 3;
+    }
+    .head-img {
+      position: absolute;
+    }
   }
+  .btn {
+    position: absolute;
+    top: calc(50% - 25px);
+    left: 40px;
+    width: 50px;
+    height: 50px;
+    background: url(../assets/btn.png);
+    transform: rotate(180deg);
+    background-size: cover;
+    &.next {
+      left: auto;
+      right: 40px;
+      transform: rotate(0deg);
+    }
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,63 +1,99 @@
 <template>
   <div class="home">
-	  <div class="operation-header">
-	    <img src="http://mat1.gtimg.com/bbs/static/20190906-logo2.png" class="h-title"></img>
-	    <div class="h">领取你的国庆专属头像</div>
-	    <div class="hh">目前已经有<span>14000万</span>人参与活动</div>
-	  </div>
-    <slideshow></slideshow>
+    <div class="operation-header">
+      <img
+        src="http://mat1.gtimg.com/bbs/static/20190906-logo2.png"
+        class="h-title"
+      />
+      <div class="h">领取你的国庆专属头像</div>
+      <div class="hh">目前已经有<span>14000万</span>人参与活动</div>
+    </div>
+    <slideshow :borderImgList="borderImgList"></slideshow>
+    <div class="btn-wapper">
+    	<div class="btn-com upload">上传图片</div>
+    	<div class="btn-com save">保存头像</div>
+    </div>
   </div>
 </template>
 
 <script>
-import slideshow from '../components/slideshow'
+import slideshow from "../components/slideshow";
 export default {
   name: "home",
   components: {
-  	slideshow
+    slideshow
+  },
+  data() {
+    return {
+      borderImgList: [
+        { src: require("../assets/border1.png") },
+        { src: require("../assets/border2.png") },
+        { src: require("../assets/border3.png") }
+      ]
+    };
   }
 };
 </script>
 
 <style lang="scss">
-.home{
-	position: relative;
-	overflow-x: hidden;
-	width: 100%;
-	min-height: 100vh;
-	max-width: 500px;
-	margin: 0 auto;
-	background-image: url(../assets/bg.png);
-	background-repeat: no-repeat;
-	background-size: cover;
-	.operation-header{
-		width: 100%;
-		margin-top: 1.8rem;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		.h-title{
-			margin-top: 4rem;
-			width: 80%;
-		}
-		.h{
-			color: #FFFEBB;
-			font-size: 24px;
-		}
-		.hh{
-			margin-top: 1.5rem;
-			color: #fffcf7;
-			font-size: medium;
-			span{
-				margin: 0 5px 0 5px;
-				background-color: #ea4726;
-				color: #ffd697;
-				padding: 5px;
-				border-radius: 15px;
-			}
-		}
-	}
-
+.home {
+  position: relative;
+  overflow-x: hidden;
+  width: 100%;
+  min-height: 100vh;
+  max-width: 500px;
+  margin: 0 auto;
+  background-image: url(../assets/bg.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  .operation-header {
+    width: 100%;
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .h-title {
+      margin-top: 4rem;
+      width: 80%;
+    }
+    .h {
+      color: #fffebb;
+      font-size: 22px;
+    }
+    .hh {
+      margin-top: 1.5rem;
+      color: #fffcf7;
+      font-size: medium;
+      span {
+        margin: 0 5px 0 5px;
+        background-color: #ea4726;
+        color: #ffd697;
+        padding: 5px;
+        border-radius: 15px;
+      }
+    }
+  }
+  .btn-wapper{
+  	padding: 40px;
+  	.btn-com{
+  		float: left;
+  		margin-left: 15px;
+  		height: 45px;
+  		width: calc(50% - 30px);
+  		border-radius: 20px;
+  		text-align: center;
+  		line-height: 45px;
+  		font-size: 20px;
+  		&.upload{
+  			background: #fff;
+  			color: #c45700;
+  		}
+  		&.save{
+  			background: #fed677;
+  			color: #c45700;
+  		}
+  	}
+  }
 }
 </style>
